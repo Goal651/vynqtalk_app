ec6d926ec05519bafe5c2320c82175a7093616d1
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2025-12-09 15:33:26 +0200

    Initial project setup for vynqtalk app
    Added Xcode project files, asset catalogs, main SwiftUI app entry point, basic ContentView, and initial unit/UI test stubs for the vynqtalk iOS application.

---
+++ vynqtalk/vynqtalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  vynqtalkApp.swift
+//  vynqtalk
+//
+//  Created by wigothehacker on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct vynqtalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vynqtalkTests/vynqtalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  vynqtalkTests.swift
+//  vynqtalkTests
+//
+//  Created by wigothehacker on 12/9/25.
+//
+
+import Testing
+@testable import vynqtalk
+
+struct vynqtalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
